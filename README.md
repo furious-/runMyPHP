97ec71dcf3b4853bbdf8d41b972961aa250f55a1
FURiOUS <furious@filewarez.tv>
2013-11-16 00:40:09 -0200

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
-runmyphp
-========
+# runMyPHP
+Run your PHP code directly on SublimeText 3 and see the results into output panel
 
-Plugin for Sublime Text 3 that runs PHP code and shows in an output panel
+## Installation
+See bellow how to install this plugin
+
+### Install using git
+To install this Plugin via git, simply browse to your 'Packages' folder like this:
+
+**for Windows**
+> cd %APPDATA%/Sublime Text 3/Packages
+
+**for OS X**
+> cd ~/Library/Application Support/Sublime Text 3/Packages
+
+**for Linux**
+> cd ~/.Sublime Text 2/Packages
+
+**for Portable Installations**
+> cd PATH_TO_PORTABLE_INSTALLATION/Sublime Text 3/Data/Packages
+
+**and clone this repository**
+> git clone https://github.com/furious-/runmyphp
+
+
+### Install manually
+* Download the files using the GitHub .zip download option
+* Unzip the files and rename the folder to 'runMyPHP'
+* Copy the folder to your Sublime Text 3 'Packages' directory
+
+## Settings
+The following settings are available and optional, but the default settings should be mostly what you want if you install this plugin
+	
+	{
+		"php_path": "YOUR_PHP_BINARY_PATH",
+	}
+
+**php_path**: The path to your PHP binary (fallback: "php"-call)
+
+## Usage
+If the PHP syntax is active, select the piece of code you want to get an output from, or if you want the whole file be used left the selection blank, and press this shortcut:
+
+	Ctrl+Shift+X
+
+Rightclick in the document and you can do the same above
+
+## Release Notes
+This is an initial version, so can have some bugs. And it's my first time making a plugin for ST3, and my second day programming python :P
+
+### Contact
+If you liked this plugin, let me know:
+
+* Twitter: [@furious_](http://twitter.com/furious_)
+* Website: <http://furious.cc>
---
+++ runMyPHP.py
@@ -0,0 +1,76 @@
+##
+# runMyPHP - SublimeText 3 plugin
+# Coder: furious (github.com/furious-)
+##
+
+import sublime, sublime_plugin, threading, subprocess, os, base64
+
+class runMyPHP(threading.Thread):
+	def __init__(self, code):
+		self.code = code
+		self.view = sublime.active_window().active_view()
+		threading.Thread.__init__(self)
+
+	def run(self):
+		self.execute(self.code)
+
+	# process and return encoded code
+	def process(self, rawcode):
+		return "eval(base64_decode('" + base64.encodestring(bytes(rawcode.replace('<?', '').replace('?>', ''), 'ascii')).decode('ascii').replace("\n", '') + "'));"
+
+	# execute code, print output
+	def execute(self, code):
+		if self.view.settings().has('php_path'):
+			path = os.path.realpath(self.view.settings().get('php_path')) + os.sep
+		else: path = ''
+
+		command = '"%(path)sphp" -r "%(code)s"' % {"path": path, "code": self.process(code)}
+
+		self.output({"output": "Processing...", "title": "runMyPHP"})
+		#php = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+		#result, err = php.communicate()
+		result = subprocess.check_output(command, stderr=subprocess.STDOUT, universal_newlines=True, shell=True)
+		self.output({"output": result})
+		sublime.status_message('Done.')
+
+	# output workaround, thanks ST3 ¬¬'
+	def output(self, args):
+		self.view.window().run_command("runmyphpoutput", args)
+
+class RunmyphpCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		self.edit = edit
+		
+		selection = ""
+		for sel in self.view.sel():
+			selection += self.view.substr(sel)
+
+		if len(selection) == 0:
+			selection = self.view.substr(sublime.Region(0, self.view.size()))
+		
+		if self.show():
+			sublime.status_message('Processing your code...')
+			thread = runMyPHP(selection)
+			thread.start()
+		else:
+			sublime.status_message('PHP code not detected.')
+
+	def show(self):
+		view = sublime.active_window().active_view()
+		if view:
+			return view.settings().get('syntax') == 'Packages/PHP/PHP.tmLanguage'
+
+# output workaround, thanks ST3 ¬¬'
+class RunmyphpoutputCommand(sublime_plugin.TextCommand):
+	def run(self, edit, output, multiline = False, title = 'Results:', panel_name = 'runMyPHP'):
+		self.edit = edit
+		if multiline or "\n" in output:
+			panel = self.view.window().create_output_panel(panel_name)
+			panel.set_read_only(False)
+			panel.set_syntax_file('Packages/Text/Plain text.tmLanguage')
+			panel.insert(self.edit, panel.size(), title + "\n" + output)
+			panel.set_read_only(True)
+			self.view.window().run_command("show_panel", {"panel": "output." + panel_name})
+		else:
+			self.view.window().show_input_panel(title, output, None, None, None)
+		
